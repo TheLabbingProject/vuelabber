f690782e9349b69a8e3c4c6d6210e39af13246e2
Noam Aharony <aharonyn@gmail.com>
2020-07-13 15:38:34 +0300

    Added the dotenv configuration for the reading of .env files in vuelabber.

--- src/main.js
+++ src/main.js
@@ -10,11 +10,13 @@ import Vuelidate from 'vuelidate'
 import VueMermaid from 'vue-mermaid'
 import vuetify from './plugins/vuetify'
 import VS2 from 'vue-script2'
+import { required } from 'vuelidate/lib/validators'
 
 Vue.config.productionTip = false
 Vue.use(Vuelidate)
 Vue.use(VueMermaid)
 Vue.use(VS2)
+require('dotenv').config()
 
 new Vue({
   router,
