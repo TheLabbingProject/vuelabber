6be057759ab93d744680f7e7a7f1123e4719b76b
Zvi Baratz <z.baratz@gmail.com>
2021-10-13 11:25:00 +0300

    Refactored app template.

--- src/App.vue
+++ src/App.vue
@@ -4,128 +4,166 @@
     <v-navigation-drawer v-model="drawer" clipped absolute overflow app>
       <v-list dense>
         <!-- Home -->
-        <v-list-item to="/home" class="drawer-link">
+        <v-list-item :to="homeItem.destination" class="drawer-link">
           <v-list-item-icon>
-            <v-icon>home</v-icon>
+            <v-icon>
+              {{ homeItem.icon }}
+            </v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>
+              {{ homeItem.label }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <!-- Studies -->
-        <v-list-item to="/studies" class="drawer-link">
+        <v-list-item :to="studiesItem.destination" class="drawer-link">
           <v-list-item-icon>
-            <v-icon>star</v-icon>
+            <v-icon>
+              {{ studiesItem.icon }}
+            </v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>Studies</v-list-item-title>
+            <v-list-item-title>
+              {{ studiesItem.label }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <!-- Subjects -->
-        <v-list-item to="/subjects" class="drawer-link">
+        <v-list-item :to="subjectsItem.destination" class="drawer-link">
           <v-list-item-icon>
-            <v-icon>people</v-icon>
+            <v-icon>
+              {{ subjectsItem.icon }}
+            </v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>Subjects</v-list-item-title>
+            <v-list-item-title>
+              {{ subjectsItem.label }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <!-- MRI Browser -->
-        <v-list-item to="/mri-browser" class="drawer-link">
+        <v-list-item :to="mriBrowserItem.destination" class="drawer-link">
           <v-list-item-icon>
-            <v-icon>blur_on</v-icon>
+            <v-icon>
+              {{ mriBrowserItem.icon }}
+            </v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>MRI Browser</v-list-item-title>
+            <v-list-item-title>
+              {{ mriBrowserItem.label }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
-        <!-- Data -->
-        <v-list-group no-action prepend-icon="storage" :value="false">
-          <template v-slot:activator>
-            <v-list-item-title class="drawer-link">Data</v-list-item-title>
-          </template>
-
-          <!-- MRI -->
-          <v-list-group no-action sub-group :value="false">
-            <template v-slot:activator>
-              <v-list-item class="drawer-link pr-0">
-                <v-list-item-title>MRI</v-list-item-title>
-                <v-list-item-icon>
-                  <v-icon>blur_on</v-icon>
-                </v-list-item-icon>
-              </v-list-item>
-            </template>
-
-            <!-- Sequence Types -->
-            <v-list-item to="/mri-sequence-types" class="drawer-link">
-              <v-list-item-title>Sequence Types</v-list-item-title>
-              <v-list-item-icon>
-                <v-icon>list</v-icon>
-              </v-list-item-icon>
-            </v-list-item>
-
-            <!-- DICOM -->
-            <v-list-item to="/dicom-browser" class="drawer-link">
-              <v-list-item-title>DICOM</v-list-item-title>
-              <v-list-item-icon>
-                <v-icon>mdi-remote-desktop</v-icon>
-              </v-list-item-icon>
-            </v-list-item>
-
-            <!-- BrainBrowser -->
-            <!-- <v-list-item to="/brain-browser" class="drawer-link">
-              <v-list-item-title>Brain Browser</v-list-item-title>
-              <v-list-item-icon>
-                <v-icon>mdi-brain</v-icon>
-              </v-list-item-icon>
-            </v-list-item> -->
-          </v-list-group>
-        </v-list-group>
-
         <!-- Analyses -->
-        <v-list-group no-action prepend-icon="equalizer">
+        <v-list-group no-action :prepend-icon="analysesItem.icon">
           <template v-slot:activator>
-            <v-list-item-title class="drawer-link">Analyses</v-list-item-title>
+            <v-list-item-title class="drawer-link">
+              {{ analysesItem.label }}
+            </v-list-item-title>
           </template>
 
           <!-- Browse -->
-          <v-list-item to="/analysis-browser" class="drawer-link">
-            <v-list-item-title>Browse</v-list-item-title>
+          <v-list-item
+            :to="analysesBrowserItem.destination"
+            class="drawer-link"
+          >
+            <v-list-item-title>
+              {{ analysesBrowserItem.label }}
+            </v-list-item-title>
             <v-list-item-icon>
-              <v-icon>search</v-icon>
+              <v-icon>
+                {{ analysesBrowserItem.icon }}
+              </v-icon>
             </v-list-item-icon>
           </v-list-item>
 
           <!-- Runs -->
-          <v-list-item to="/run-browser" class="drawer-link">
-            <v-list-item-title>Runs</v-list-item-title>
+          <v-list-item :to="runsItem.destination" class="drawer-link">
+            <v-list-item-title>
+              {{ runsItem.label }}
+            </v-list-item-title>
             <v-list-item-icon>
-              <v-icon>format_list_bulleted</v-icon>
+              <v-icon>
+                {{ runsItem.icon }}
+              </v-icon>
             </v-list-item-icon>
           </v-list-item>
 
           <!-- Pipelines -->
-          <v-list-item to="/pipeline-browser" class="drawer-link">
-            <v-list-item-title>Pipelines</v-list-item-title>
+          <v-list-item :to="pipelinesItem.destination" class="drawer-link">
+            <v-list-item-title>
+              {{ pipelinesItem.label }}
+            </v-list-item-title>
             <v-list-item-icon>
-              <v-icon>timeline</v-icon>
+              <v-icon>
+                {{ pipelinesItem.icon }}
+              </v-icon>
             </v-list-item-icon>
           </v-list-item>
         </v-list-group>
 
         <!-- Tasks -->
-        <v-list-item to="/tasks" class="drawer-link">
+        <v-list-item :to="tasksItem.destination" class="drawer-link">
           <v-list-item-icon>
-            <v-icon>timer</v-icon>
+            <v-icon>
+              {{ tasksItem.icon }}
+            </v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>Tasks</v-list-item-title>
+            <v-list-item-title>
+              {{ tasksItem.label }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        <!-- Advanced Options -->
+        <v-list-group
+          no-action
+          :prepend-icon="advancedItem.icon"
+          :value="false"
+        >
+          <template v-slot:activator>
+            <v-list-item-title class="drawer-link">
+              {{ advancedItem.label }}
+            </v-list-item-title>
+          </template>
+
+          <!-- Sequence Types -->
+          <v-list-item :to="sequenceTypesItem.destination" class="drawer-link">
+            <v-list-item-title>
+              {{ sequenceTypesItem.label }}
+            </v-list-item-title>
+            <v-list-item-icon>
+              <v-icon>
+                {{ sequenceTypesItem.icon }}
+              </v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+
+          <!-- DICOM -->
+          <v-list-item :to="dicomBrowserItem.destination" class="drawer-link">
+            <v-list-item-title>
+              {{ dicomBrowserItem.label }}
+            </v-list-item-title>
+            <v-list-item-icon>
+              <v-icon>
+                {{ dicomBrowserItem.icon }}
+              </v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+
+          <!-- BrainBrowser -->
+          <!-- <v-list-item to="/brain-browser" class="drawer-link">
+              <v-list-item-title>Brain Browser</v-list-item-title>
+              <v-list-item-icon>
+                <v-icon>mdi-brain</v-icon>
+              </v-list-item-icon>
+            </v-list-item> -->
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
 
@@ -135,7 +173,7 @@
 
       <!-- Title -->
       <v-toolbar-title>
-        pylabber
+        {{ title }}
       </v-toolbar-title>
 
       <v-spacer />
@@ -150,15 +188,15 @@
                   v-if="user.profile && user.profile['image']"
                   :src="user.profile['image']"
                 />
-                <img v-else src="/user.png" />
+                <img v-else :src="defaultUserImage" />
               </v-avatar>
               &nbsp; {{ user.username }}
             </v-btn>
           </template>
           <v-list>
-            <router-link to="/logout" class="dropmenu-link">
+            <router-link :to="logoutItem.destination" class="dropmenu-link">
               <v-list-item>
-                Logout
+                {{ logoutItem.label }}
               </v-list-item>
             </router-link>
           </v-list>
@@ -185,7 +223,63 @@ import { mapGetters, mapState } from 'vuex'
 
 export default {
   data: () => ({
-    drawer: {}
+    title: 'pylabber',
+    drawer: {},
+    homeItem: { label: 'Home', icon: 'home', destination: '/home' },
+    studiesItem: { label: 'Studies', icon: 'star', destination: '/studies' },
+    subjectsItem: {
+      label: 'Subjects',
+      icon: 'people',
+      destination: '/subjects'
+    },
+    mriBrowserItem: {
+      label: 'MRI Browser',
+      icon: 'blur_on',
+      destination: '/mri-browser'
+    },
+    advancedItem: {
+      label: 'Advanced',
+      icon: 'settings'
+    },
+    analysesItem: {
+      label: 'Analyses',
+      icon: 'equalizer'
+    },
+    analysesBrowserItem: {
+      label: 'Browser',
+      icon: 'search',
+      destination: '/analysis-browser'
+    },
+    runsItem: {
+      label: 'Runs',
+      icon: 'format_list_bulleted',
+      destination: '/run-browser'
+    },
+    pipelinesItem: {
+      label: 'Pipelines',
+      icon: 'timeline',
+      destination: '/pipeline-browser'
+    },
+    tasksItem: {
+      label: 'Tasks',
+      icon: 'timer',
+      destination: '/tasks'
+    },
+    sequenceTypesItem: {
+      label: 'MRI Sequence Types',
+      icon: 'list',
+      destination: '/mri-sequence-types'
+    },
+    dicomBrowserItem: {
+      label: 'DICOM Browser',
+      icon: 'mdi-remote-desktop',
+      destination: '/dicom-browser'
+    },
+    logoutItem: {
+      label: 'Logout',
+      destination: '/logout'
+    },
+    defaultUserImage: '/user.png'
   }),
   computed: {
     ...mapGetters('auth', ['isAuthenticated']),
