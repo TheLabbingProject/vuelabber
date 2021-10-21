e23446b456c188ad5e4ed02e9550da3b1d70e4db
Zvi Baratz <z.baratz@gmail.com>
2021-10-21 14:58:21 +0300

    Changed subject table containers to be fuild.

--- src/components/research/subject-table-controls.vue
+++ src/components/research/subject-table-controls.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-col>
       <v-row>
         <v-col :cols="1">
--- src/components/research/subject-table.vue
+++ src/components/research/subject-table.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-row>
       <div class="title text-left">
         {{ title }}
